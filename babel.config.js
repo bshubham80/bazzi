d363e26fa835448b5c1481cf2181cabdb80a6893
Shubham Bansal <shubham.bansal@betheshyft.com>
2023-06-06 13:38:28 +0530

    Added file structure

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./__tests__/'],
+          '@components': './src/components',
+          '@screens': './src/screens',
+        },
+      },
+    ],
+  ],
 };
